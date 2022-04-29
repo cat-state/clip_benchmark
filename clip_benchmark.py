6cfdea0f781b88347d0f1f7dfc502c0fdcb49550
cat-state <cat-state@tutanota.com>
2022-04-29 15:33:43 +0000

    ploti t

--- clip_benchmark.py
+++ clip_benchmark.py
@@ -20,11 +20,38 @@ def get_sentence_embs(path: Path, dataset: Path):
         return sentence_embs
 
 
-def clip_benchmark(img_embeds_file: Path,
-                   text_embeds_file: Path,
-                   sentence_embs: Path,
-                   n=30000,
-                   dataset="/home/a/mscoco/{00000..00059}.tar"):
+def knn(queries, keys, k=5):
+    index = IndexFlatIP(keys.shape[1])
+    index.add(keys)
+    _, idxs = index.search(queries, k=k)
+    return idxs
+
+def cross_modal_retrieval(
+    img_embeds: np.array,
+    text_embeds: np.array,
+    k=5
+):
+    subset_img = img_embeds
+    subset_txt = text_embeds
+    nearest_imgs_to_txt = knn(subset_txt, keys=subset_img, k=k)
+    nearest_txts_to_img = knn(subset_img, keys=subset_txt, k=k)
+
+    return { 
+        "text->img": (nearest_imgs_to_txt == np.arange(len(text_embeds))[:, None]),
+        "img->text": (nearest_txts_to_img == np.arange(len(img_embeds))[:, None]),
+        "text->img-idxs": nearest_imgs_to_txt[:, 0],
+        "img->text-idxs": nearest_txts_to_img[:, 0]
+    }
+
+def clip_benchmark(
+    img_embeds_file: Path,
+    text_embeds_file: Path,
+    sentence_embs: Path,
+    n=30000,
+    k=5,
+    dataset="/home/a/mscoco-1st-cap/{00000..00011}.tar",
+    title=None
+):
 
 
     sentence_embs = get_sentence_embs(sentence_embs, dataset)
@@ -33,34 +60,110 @@ def clip_benchmark(img_embeds_file: Path,
     gt = np.arange(len(img_embeds))[:, None]
     text_embeds = np.load(text_embeds_file)[:n].astype(np.float32)
 
-    unique_img_embeds, unique_imgs, unique_img_idx = np.unique(img_embeds, return_index=True, return_inverse=True, axis=0)
-    unique_text_embeds, unique_texts, unique_text_idx = np.unique(text_embeds, return_index=True, return_inverse=True, axis=0)
-    print("num unique img embeds", len(unique_img_embeds))
-    print("num unique text embeds", len(unique_text_embeds))
-    img_gt = gt[:len(unique_img_embeds)]
-    text_gt = gt[:len(unique_text_embeds)]
+    n_range = np.geomspace(5000, n, num=6, dtype=np.int32).tolist()
+
+    results = (cross_modal_retrieval(img_embeds[:first_n],
+                                     text_embeds[:first_n],
+                                     k=k) for first_n in n_range)
+
+    summary = ({k:
+                { **r, "text->img": r["text->img"].any(axis=1),
+                  "img->text": r["img->text"].any(axis=1)},
+                1:
+                {**r, "text->img": r["text->img"][:, 0],
+                 "img->text": r["img->text"][:, 0]}}
+               for r in results)
+
+    def dot(x, y):
+        return (x * y).sum(axis=-1)
+
+    similarity = ({**r,
+                   "similarity": {
+                       "text->img": dot(sentence_embs[:first_n][r[1]["text->img-idxs"]], sentence_embs[:first_n]),
+                       "img->text": dot(sentence_embs[:first_n][r[1]["img->text-idxs"]], sentence_embs[:first_n])
+                   }}
+        for first_n, r in zip(n_range, summary))
+
+    def report(results):
+        for first_n, s in zip(n_range, results):
+            print("n:", first_n)
+            for top_k in [1, k, "similarity"]:
+                for metric in ["text->img","img->text"]:
+                    print(f"{metric} @ {top_k} {s[top_k][metric].mean():.3f}",
+
+                          f"{f'{s[top_k][metric].std():.3f}' if top_k == 'similarity' else ''}")
+                    yield {"first_n": first_n,
+                           "metric": metric,
+                           "top_k": top_k,
+                           "measuring": "similarity" if top_k == "similarity" else "knn",
+                           "@": top_k,
+                           "value": s[top_k][metric].mean(),
+                           "value-std": s[top_k][metric].std()}
 
 
-    img_brute_index = IndexFlatIP(unique_img_embeds.shape[1])
-    img_brute_index.add(unique_img_embeds)
+    import pandas
 
-    text_brute_index = IndexFlatIP(unique_text_embeds.shape[1])
-    text_brute_index.add(unique_text_embeds)
-    print("searching brute force")
+    return pandas.DataFrame.from_records([*report(similarity)])
+    print(results)
+    ax = seaborn.lineplot(x="first_n", y="value", hue="metric", data=results[results['measuring'] == 'knn'])
+    ax.set(ylabel="cross-modal recall")
+    plt.legend()
+    plt.title(title)
+    if title is not None:
+        plt.savefig(f"{title}.png")
+    plt.show()
 
-    img_dists, img_idxs = img_brute_index.search(text_embeds[unique_imgs], 5)
-    text_dists, text_idxs = text_brute_index.search(img_embeds[unique_texts], 5)
+def plot_models():
+    import pandas
+    import seaborn
 
-    print("text->img retrieval @5", (img_idxs == img_gt).any(axis=-1).sum() / len(img_idxs))
-    print("img->text retrieval @5", (text_idxs == text_gt).any(axis=-1).sum() / len(text_idxs))
+    import matplotlib.pyplot as plt
+    models = {
+        "openclip ViT-B-16": dict(img_embeds="coco-embeds-open-clip-vit-b-16/img_emb/img_emb_0.npy",
+                                  text_embeds="coco-embeds-open-clip-vit-b-16/text_emb/text_emb_0.npy"),
+        "openclip ViT-B-32": dict(img_embeds="coco-embeds-open-clip-vit-b-32/img_emb/img_emb_0.npy",
+                                  text_embeds="coco-embeds-open-clip-vit-b-32/text_emb/text_emb_0.npy"),
+        "openai ViT-B/16":  dict(img_embeds="coco-embeds-openai-vit-b-16/img_emb/img_emb_0.npy",
+                                  text_embeds="coco-embeds-openai-vit-b-16/text_emb/text_emb_0.npy"),
+        "openai ViT-B/32":  dict(img_embeds="coco-embeds-openai-vit-b-32/img_emb/img_emb_0.npy",
+                                  text_embeds="coco-embeds-openai-vit-b-32/text_emb/text_emb_0.npy"),
+        "openai ViT-L/14":  dict(img_embeds="coco-embeds-openai-vit-l-14/img_emb/img_emb_0.npy",
+                                  text_embeds="coco-embeds-openai-vit-l-14/text_emb/text_emb_0.npy"),
+        "cloob ViT-B/16":  dict(img_embeds="cloob-vit-b-16/cloob_laion_400m_vit_b_16_32_epochs_coco_train2017_image_embeds.npy",
+                                  text_embeds="cloob-vit-b-16/cloob_laion_400m_vit_b_16_32_epochs_coco_train2017_text_embeds.npy"),
+        "cloob ViT-L/14":  dict(img_embeds="cloob-vit-l-14-168_image_embeds.npy",
+                                text_embeds="cloob-vit-l-14-168_text_embeds.npy")
+    }
 
-    text_sim = (sentence_embs[unique_imgs[img_idxs]] * sentence_embs[unique_imgs][:, None, :]).sum(axis=-1)
-    print("text->img similarity @5:", text_sim.max(1).mean(), text_sim.max(1).std())
-    print("text->img similarity @1:", text_sim[:, 0].mean(), text_sim[:, 0].std())
+    results = pandas.DataFrame()
+    for name, embs in models.items():
+        print(name)
+        print("-" * 50)
+        res = clip_benchmark(img_embeds_file=embs["img_embeds"],
+                             text_embeds_file=embs["text_embeds"], n=50000, k=5, sentence_embs="sent-1st.npy", title=name,
+                       dataset="../mscoco-1st-cap/{00000..00011}.tar")
+        res["model"] = name
+        results = pandas.concat([results, res], ignore_index=True)
 
-    text_sim = (sentence_embs[unique_texts[text_idxs]] * sentence_embs[unique_texts][:, None, :]).sum(axis=-1)
-    print("img->text similarity @5:", text_sim.max(1).mean(), text_sim.max(1).std())
-    print("img->text similarity @1:", text_sim[:, 0].mean(), text_sim[:, 0].std())
+    print(results)
+    fig, axes = plt.subplots(2, 2)
+
+    for i, metric in enumerate(["text->img", "img->text"]):
+        for j, at in enumerate([5, "similarity"]):
+            lims = (results[results["@"] == at]["value"].min() * 0.95, results[results["@"] == at]["value"].max() * 1.05)
+            for ax in axes[j]:
+                print(lims)
+                ax.set(ylim=lims)
+            subset = results[results['metric'] == metric]
+            subset = subset[subset["@"] == at]
+            title = "similarity" if at == "similarity" else f"retrieval@{at}"
+            ax = seaborn.lineplot(x="first_n", y="value", hue="model", data=subset, ax=axes[j, i])
+            ax.set(ylabel=f"{title}", title=f"{metric} {title}")
+
+
+    plt.show()
+
+plot_models()
 
 
 
